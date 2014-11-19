02b88ede4ff6c90fe8adcbf9bf6d4e0a7e094bc5
ilynne <ilynne@gmail.com>
2014-11-19 15:30:34 -0800

    adds lynne button and bonus javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,18 @@
 //Function called "signIn", causes an alert to pop up with text "Thanks for signing in". Should be triggered by clicking on the button with id of "sign-in".
 
 function signIn(){
-    alert("Thanks for signing in")
+  alert("Thanks for signing in")
 }
 
 //Function called "signUp", causes a prompt to appear and ask for user's email address, then prints to the "output-here" paragraph "Thanks for signing up, " and the entered email address. Should be triggered by clicking on the button with id of "sign-up".
 
 function signUp(){
-    var email = prompt("Please enter your email address", "email@example.com");
-    document.getElementById('output-here').innerHTML = "Thanks for signing up " + email;
+  var email = prompt("Please enter your email address", "email@example.com");
+  document.getElementById('output-here').innerHTML = "Thanks for signing up " + email;
+}
+
+//Extra function for Lynne's button
+
+function orangeYouGlad() {
+  document.getElementById('output-here').innerHTML = "Orange you glad that you clicked that button?";
 }
